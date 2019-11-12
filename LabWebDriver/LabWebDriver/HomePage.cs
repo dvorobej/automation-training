30eb865787a7aa119c0a9f6710c23898d3b0bf0c
Dmitry Vorobey <yagamilit@gmail.com>
2019-11-12 17:38:46 +0300

    lab 2 and lab 4

---
+++ LabWebDriver/LabWebDriver/HomePage.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWebDriver
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.XPath, Using = "/html/body/div[1]/div[1]/header/div/div/div/div[3]/div/div[1]/button")]
+        public IWebElement TitleSignIn { get; set; }
+           
+        [FindsBy(How = How.Name, Using = "username")]
+        public IWebElement Email { get; set; }
+
+        [FindsBy(How = How.Name, Using = "passwd")]
+        public IWebElement Password { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "button.submit[aria-label='Sign in']")]
+        public IWebElement ButtonSignIn { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@aria-label='Select your country and language. Current country: United States']")]
+        public IWebElement Language { get; set; }
+       
+        [FindsBy(How = How.XPath, Using = "//*[@data-locale='ru-RU']")]
+        public IWebElement TitleLanguage { get; set; }
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("https://www.kayak.com/flights");
+        }
+
+        public void SignIn()
+        {
+            TitleSignIn.Click();
+        }
+
+        public void EntryEmail(string textToType)
+        {
+            Email.Click();
+            Email.SendKeys(textToType);
+        }
+
+        public void EntryPassword(string textToType)
+        {
+            Password.Click();
+            Password.SendKeys(textToType);
+            ButtonSignIn.Click();
+        }
+
+        public void ChooseLanguage()
+        {
+            Language.Click();
+            TitleLanguage.Click();
+            
+        }
+
+
+    }
+}
---
+++ LabWebDriver/WebDriverTest/HomePageTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using LabWebDriver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebDriverTest
+{
+    [TestClass]
+    public class HomePageTest
+    {
+        public const string EMAIL = "example@gmail.com";
+        public const string PASSWORD = "example12345678";
+
+        public IWebDriver Driver { get; set; }
+        public WebDriverWait Wait { get; set; }
+
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.Driver = new ChromeDriver();
+            this.Driver.Manage().Window.Maximize();
+            this.Wait = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(30));
+
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.Driver.Quit();
+        }
+
+        [TestMethod]
+        public void SignInTest()
+        {
+            HomePage homePage = new HomePage(this.Driver);
+            homePage.goToPage();
+            homePage.SignIn();
+            homePage.EntryEmail(EMAIL);
+            homePage.EntryPassword(PASSWORD);
+        }
+
+      [TestMethod]
+        public void Search()
+        {
+            HomePage homePage = new HomePage(this.Driver);
+            homePage.goToPage();
+            homePage.ChooseLanguage();
+        }
+    }
+}
