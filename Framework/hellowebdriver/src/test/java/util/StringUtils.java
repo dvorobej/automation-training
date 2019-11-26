bea38bdfdc7f023420ea740569fff67255979efb
Dmitry Vorobey <yagamilit@gmail.com>
2019-11-26 22:48:48 +0300

    some changes

--- Framework/hellowebdriver/src/test/java/util/StringUtils.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class StringUtils {
-    public static final String SITE_HOMEPAGE_URL = "https://www.kayak.com/flights";
-    public static final String LOGIN_ALLERT_MESSAGE = "Incorrect password.";
-    public static final String INCORRECT_PASSWORD = "dfaS12345";
-    public static final String ZERO_ADULT_ALLERT_MESSAGE = "Searches need at least 1 traveler";
-}
